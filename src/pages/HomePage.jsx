80cdf4716e2dc3c94fe21d0ed20afd5c4f9dda27
Keaton Nemes <kknemes@gmail.com>
2025-02-02 18:11:47 -0600

    Roughed out HomePage in need of some refinement

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useOutletContext } from 'react-router-dom';
+import './HomePage.css';
 
 function Home() {
-    const { pencilColor } = useOutletContext();
 
     return (
         <div className="page-content layout-main">
-            <h1>Home</h1>
-            <p>Welcome to the home page!</p>
-            <p>Current Pencil Color: <span style={{ color: pencilColor }}>{pencilColor}</span></p>
+            <section id="home">
+                <h1>Welcome to my online sketchbook.</h1>
+                <p>
+                    I'm a <b>computational designer</b>, <b>creative coder</b>, and aspiring <b>woodworker</b> passionate about the
+                    intersection of <b>design</b> and <b>mathematics</b>. Driven by a love of
+                    <b> learning</b> and <b>building</b> new things, I explore the interplay of
+                    <b> creativity</b>, <b>technology</b>, and <b>craft</b>.
+                </p>
+                <p>
+                    Here, you'll find a collection of my work, from
+                    <b> computational explorations</b> to <b>creative coding projects</b>.
+                    Feel free to <b>annotate</b> anywhere directly using the draw palette.
+                    Any annotations you create can be saved and sent to me via the
+                    <b> contact</b> page - I'm eager to see your <b>perspective</b>!
+                </p>
+                <p>
+                    I believe in a <b>rigorous</b>, <b>fast-footed</b> approach to
+                    <b>problem-solving</b>, and I hope this sketchbook provides valuable
+                    insights into my <b>design philosophy</b>.
+                </p>
+                <a href="/about" className="button">Learn More</a>
+            </section>
         </div>
     );
 }
