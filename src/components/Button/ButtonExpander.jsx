8e4a8de2cc978ba0d5aff7ff97a96b2cb06b21f6
Keaton Nemes <kknemes@gmail.com>
2025-02-22 22:13:09 -0600

    Starting to stylize draw button

--- src/components/Button/ButtonExpander.jsx
+++ src/components/Button/ButtonExpander.jsx
@@ -1,69 +1,42 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import './ButtonExpander.css';
+import PencilIcon from '../../assets/SVG/PencilIcon.svg';
 
-function ButtonExpander({ children, isDrawing, setIsDrawing, expandDirection = 'left', toggleLabel = '>', collapseLabel = '<' }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [contentWidth, setContentWidth] = useState(0)
-    const contentRef = useRef(null);
-    const toggleRef = useRef(null);
+function ButtonExpander({ children, isDrawing, setIsDrawing, toggleLabel = '>', collapseLabel = '<' }) {
+    const [isPaletteOpen, setIsPaletteOpen] = useState(false);
+    const paletteRef = useRef(null);
 
-    const setToggleRef = useCallback((node) => {
-        toggleRef.current = node;
-    }, []);
-
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
-        setIsDrawing(!isDrawing);
+    const togglePalette = () => {
+        setIsPaletteOpen(!isPaletteOpen);
+        if (!isPaletteOpen) {
+            setIsDrawing(true)
+        } else {
+            setIsDrawing(false)
+        }
     };
 
     useEffect(() => {
-        if (isExpanded && contentRef.current && toggleRef.current) {
-            const childrenArray = Array.from(contentRef.current.children);
+        const handleClickOutside = (event) => {
+            if (paletteRef.current && !paletteRef.current.contains(event.target) && !event.target.closest('.button-expander')) {
+                setIsPaletteOpen(false);
+            }
+        };
 
-            childrenArray.forEach((child, index) => {
-                setTimeout(() => {
-                    child.style.visibility = 'visible';
-                    child.style.transform = 'translateX(0)';
-                }, index * 100);
-            });
-        } else if (contentRef.current) {
-            const toggleWidth = toggleRef.current.offsetWidth;
-            const childrenArray = Array.from(contentRef.current.children);
-            childrenArray.forEach((child) => {
-                child.style.visibility = 'hidden';
-                child.style.transform = `translateX(${toggleWidth}px)`;
-            });
-        }
-    }, [isExpanded]);
-
-    const containerStyle = {
-        position: 'fixed',
-        bottom: '10px',
-        right: '120px',
-        display: 'flex',
-        alignItems: 'center',
-    };
-
-    const contentStyle = {
-        transition: 'transform 0.5s ease-in-out, visibility 0.5s ease-in-out',
-        visibility: isExpanded ? 'visible' : 'hidden', // Still use visibility 
-        overflow: 'hidden',
-        display: 'flex',
-        flexDirection: 'row',
-        padding: '10px',
-        whiteSpace: 'nowrap',
-        transform: isExpanded ? 'translateX(0)' : `translateX(${expandDirection === 'left' ? '100%' : '-100%'})`,
-    };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className="button-expander" style={containerStyle}>
-            <div className="expander-content" style={contentStyle} ref={contentRef}>
+        <div className="button-expander" >
+            <button className="expander-button" onClick={togglePalette}>
+                <img src={PencilIcon} alt="Drawing Tool" className="pencil-icon" />
+            </button>
+            <div ref={paletteRef} className={`palette ${isPaletteOpen ? 'open' : ''}`}>
                 {children}
             </div>
-            <button className="expander-toggle" onClick={toggleExpand} ref={setToggleRef}>
-                {isExpanded ? collapseLabel : toggleLabel}
-            </button>
         </div>
     );
 }
